6e4c2dae948b4764ace8d8bf299bb14cb6bef36c
Kyle Gough <kylegough98@gmail.com>
2022-10-16 18:46:46 +0100

    Projects structure

---
+++ projects/guessing_game/src/main.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    println!("Guess the number");
+    println!("Enter your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read input");
+
+    println!("Your guess: {guess}");
+}
